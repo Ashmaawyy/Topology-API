24d048a2c14f8c0e3ad78a0b9776e8acb0b1bb4f
Mohamed Al-Ashmawy <mohamed.25250@eng.modern-academy.edu.eg>
2021-09-24 19:56:15 +0200

    Read Json Function

--- Topology API.cpp
+++ Topology API.cpp
@@ -2,16 +2,22 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <json/json.h>
 using namespace std;
 
 class  topologies {
 
 public:
 
-    void read_topology(){
-    
-    
-    }
+    string file_components, topology, netlist_node;
+    int read_topology();
+    int write_topology();
+    int show_topologies();
+    int delete_topology(string);
+    int show_devices(string);
+    int show_devices_netlist(string, string);
+
     
 
 
@@ -19,9 +25,57 @@ public:
 };
 
 
+int topologies::read_topology()
+{ 
+    ifstream json_file;
+    json_file.open("F:\Python\Master-Micro Task 2- Topology API Store, Read & Query\Topology API\some_text.txt", ios::in);
+
+    topologies obj;
+    //json_file.read((char*)&obj.file_components, sizeof(obj.file_components));
+
+    Json::Value root;
+    ifstream config_doc("topologies.json", std::ifstream::binary);
+    config_doc >> root;
+    //cout << obj.file_components;
+    cout << root;
+
+
+
+
+    return 0;
+}
+
+int topologies::write_topology()
+{
+    return 0;
+}
+
+int topologies::show_topologies()
+{
+    return 0;
+}
+
+int topologies::delete_topology(string)
+{
+    return 0;
+}
+
+int topologies::show_devices(string)
+{
+    return 0;
+}
+
+int topologies::show_devices_netlist(string, string)
+{
+    return 0;
+}
+
+
+
 int main()
 {
     topologies object;
+    object.read_topology();
     return 0;
 }
 
